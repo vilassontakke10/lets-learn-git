4b8f288f970a7116ee16225657302a35e89b7162
vilas sontakke <vilas.pathri@gmail.com>
2022-11-30 12:27:00 +0530

    added muliply.java

---
+++ multiply.java
@@ -0,0 +1,9 @@
+public class multiply {
+    public static void main (string[] args ) {
+        int a = 4;
+        int b = 2;
+
+        system.out.println("the product is " + (a+b))
+        
+    }
+}
